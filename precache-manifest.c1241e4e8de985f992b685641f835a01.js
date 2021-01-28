e950e9c75770825bfc3f4a03d28d496123e0fa08
alii13 <shekhaliul44@gmail.com>
2021-01-29 01:14:52 +0530

    Updates

---
+++ precache-manifest.c1241e4e8de985f992b685641f835a01.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b99700db45f120010cc61bda4eec773",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "87387e90390a79bad1cb",
+    "url": "/portfolio/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "fdfdc644c4b4e656e2e5",
+    "url": "/portfolio/static/css/main.75bc9d1f.chunk.css"
+  },
+  {
+    "revision": "87387e90390a79bad1cb",
+    "url": "/portfolio/static/js/2.017d0d11.chunk.js"
+  },
+  {
+    "revision": "7404315072e7e961b2faa7b09c94ee47",
+    "url": "/portfolio/static/js/2.017d0d11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdfdc644c4b4e656e2e5",
+    "url": "/portfolio/static/js/main.ec0862d3.chunk.js"
+  },
+  {
+    "revision": "e5fc2f5a3c5ffd49831c",
+    "url": "/portfolio/static/js/runtime-main.5c8a1820.js"
+  },
+  {
+    "revision": "5efef1021ff9f1e2a3c125a903ee334f",
+    "url": "/portfolio/static/media/ali.5efef102.jpg"
+  },
+  {
+    "revision": "e7256fcc694d1fece0727bca2f4051e8",
+    "url": "/portfolio/static/media/express.e7256fcc.png"
+  },
+  {
+    "revision": "826f3e88fcc5b62e338bb5c3927df106",
+    "url": "/portfolio/static/media/line.826f3e88.png"
+  },
+  {
+    "revision": "d5054ba82120d64c99afb15c6f883c76",
+    "url": "/portfolio/static/media/mongodb.d5054ba8.png"
+  },
+  {
+    "revision": "6dcf699938b2291d1634f91adb0dded1",
+    "url": "/portfolio/static/media/node.6dcf6999.png"
+  },
+  {
+    "revision": "40389d61dc1a678884c7f6ff6bf28ee7",
+    "url": "/portfolio/static/media/rd2.40389d61.jpg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/portfolio/static/media/rjjjj.4009eef3.svg"
+  },
+  {
+    "revision": "1b453216c63aeb61385a833a8b187ee3",
+    "url": "/portfolio/static/media/women.1b453216.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.dab36705e7bda14b474c8016f39ee7ab.js"
+  "/portfolio/precache-manifest.c1241e4e8de985f992b685641f835a01.js"
 );
 
 self.addEventListener('message', (event) => {
